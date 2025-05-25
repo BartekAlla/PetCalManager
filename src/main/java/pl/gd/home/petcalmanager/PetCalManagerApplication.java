4ade8394e2529da9a548bfbed35e3b31f1204e87
BartomiejA <b.allaburda@gmail.com>
2025-05-25 20:26:52 +0200

    Init

---
+++ src/main/java/pl/gd/home/petcalmanager/PetCalManagerApplication.java
@@ -0,0 +1,13 @@
+package pl.gd.home.petcalmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetCalManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PetCalManagerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/gd/home/petcalmanager/PetCalManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.gd.home.petcalmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetCalManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
